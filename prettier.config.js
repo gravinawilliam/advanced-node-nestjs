e04c30a0639b517e1cc7eb39e430100d6c6e9e15
William Gravina <william.gravina@gcbinvestimentos.com>
2022-02-06 13:52:37 -0300

    fix: prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "avoid",
+  trailingComma: 'all',
+  arrowParens: 'avoid',
   bracketSpacing: true,
   printWidth: 120,
 };
